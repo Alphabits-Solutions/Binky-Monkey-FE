a1151efc81b7d2a4e04c3e4b5601eed36bef0ea2
vaibhavalphabits <vaibhav@alphabitssolutions@.com>
2025-03-25 17:41:56 +0530

    Designed HomePage

--- src/App.jsx
+++ src/App.jsx
@@ -1,19 +1,21 @@
 import React from "react";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
-import Dashboard from "./pages/Dashboard";
+// import Dashboard from "./pages/Dashboard";
 import ImageUpload from "./components/ImageUpload";
 import Create from "./components/Create";
 import Assignment from "./pages/Assignment";
 import ClientRender from "./pages/ClientRender";
 import GamePage from "./pages/GamePage";
 import Auth from "./pages/Auth";
+import HomeScreen from "./pages/HomeScreen";
 
 const App = () => {
   return (
     <Router>
       <Routes>
         <Route path="/auth" element={<Auth/>}/>
-        <Route path="/" element={<Dashboard />}>
+        <Route path="/" element={<HomeScreen />}>
+        {/* <Route path="/" element={<Dashboard />}> */}
           <Route path="/Image-upload" element={<ImageUpload />} />
           <Route path="/assignment" element={<Assignment />} />
           <Route path="/create" element={<Create />} />
---
+++ src/pages/HomeScreen.jsx
@@ -0,0 +1,89 @@
+import { Layout, Menu, Button, Card} from "antd";
+import pages from "../assets/icons/pages.svg";
+import layer from "../assets/icons/layer.svg";
+import assets from "../assets/icons/assets.svg";
+import audio from "../assets/icons/audio.svg";
+import objects from "../assets/icons/objects.svg";
+import more from "../assets/icons/more.svg";
+import folder from "../assets/icons/folder.svg";
+import "../assets/sass/homescreen.scss";
+
+const { Header, Content, Sider } = Layout;
+
+const activities = [
+  "Activity A",
+  "Activity B",
+  "Activity C",
+  "Activity D",
+  "Activity E",
+  "Activity F",
+  "Activity G",
+];
+
+const HomeScreen = () => {
+  return (
+    <Layout style={{ minHeight: "100vh" }}>
+        <Header className="header">
+
+        </Header>
+
+      {/* Main Content */}
+      <Layout className="main-content">
+      {/* Sidebar */}
+      <Sider width={100} className="sidebar">
+        
+        <Menu className="menu" theme="light" mode="vertical" defaultSelectedKeys={["1"]}>
+          <Menu.Item key="1" className="menuItem">
+             <img src={pages} alt=""  />
+          </Menu.Item>
+          <Menu.Item key="2" className="menuItem">
+          <img src={layer} alt="" />
+          </Menu.Item>
+          <Menu.Item key="3" className="menuItem">
+          <img src={assets} alt="" />
+          </Menu.Item>
+          <Menu.Item key="4" className="menuItem">
+          <img src={audio} alt="" />
+          </Menu.Item>
+          <Menu.Item key="5" className="menuItem">
+          <img src={objects} alt="" />
+          </Menu.Item>
+          <Menu.Item key="6" className="menuItem">
+          <img src={more} alt="" />
+          </Menu.Item>
+        </Menu>
+      </Sider>
+
+        <Content className="content">
+          <div className="content-header">
+            <h2 className="title">Activity file</h2>
+            <Button type="color" className="create-btn">
+              Create New Activity
+            </Button>
+          </div>
+
+          {/* Activity Grid */}
+          <div className="activity-grid">
+            {activities.map((activity, index) => (
+              <Card key={index} className="activity-card">
+                <img src={folder} alt="" />
+                <p>{activity}</p>
+              </Card>
+            ))}
+          </div>
+          {/* Activity Grid */}
+          <div className="activity-grid">
+            {activities.map((activity, index) => (
+              <Card key={index} className="activity-card">
+                <img src={folder} alt="" />
+                <p>{activity}</p>
+              </Card>
+            ))}
+          </div>
+        </Content>
+      </Layout>
+    </Layout>
+  );
+};
+
+export default HomeScreen;
